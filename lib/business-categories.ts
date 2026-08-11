8db2766ec78f64566cc69de12286afb988049e28
regfm-ctrl <regfm@broadcastnow.com.au>
2026-08-11 14:17:01 +1000

    Added Movies & Cinema

--- lib/business-categories.ts
+++ lib/business-categories.ts
@@ -25,6 +25,7 @@ export const BUSINESS_CATEGORIES: string[] = [
   "Jewellery",
   "Legal Services",
   "Medical / Health Services",
+  "Movies & Cinema",
   "Office Supplies",
   "Real Estate Agency",
   "Restaurant / Cafe",
